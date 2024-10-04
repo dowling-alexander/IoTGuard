50f1e2ea38deff5a15cf770ede434e7002281205
dowling-alexander <dowling.alexander.e@gmail.com>
2024-10-04 14:43:22 +1000

    Update cli.py

--- cli.py
+++ cli.py
@@ -3,3 +3,9 @@
 import argparser
 
 #need to support three modes. in the meantime when a mode is requested print I'm development
+
+def main():
+    parser = argparse.ArgumentParser(description="Welcome to IoT Guard, an IDS/IPS tool designed for IoT devices. -h for available commands")
+
+def help():
+    print("Available commands: work in progress")
