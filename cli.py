c87613644f5f1276ba6e03a555643b515752606c
dowling-alexander <dowling.alexander.e@gmail.com>
2024-10-05 07:59:57 +1000

    Update cli.py
    updated to fix lack of output.
    -removed the need for mandatory args
    -added a check to see if any args present
    -added an exit if nothing was submitted

--- cli.py
+++ cli.py
@@ -1,27 +1,35 @@
-#Command line interface for IoTGuard
+# Command line interface for IoTGuard
 
-import argparser
+import argparse
 import os
-#need to support three modes. in the meantime when a mode is requested print I'm development
+import sys
+
+
+# need to support three modes. in the meantime when a mode is requested print I'm in development
 
 def main():
-    #parser object
+    # parser object
+
     parser = argparse.ArgumentParser(description="Welcome to IoT Guard, an IDS/IPS tool designed for IoT devices")
 
-    
-    parser.add_argument("-w", "--watch", type = str, nargs = 1,
-                        metavar = "watch_traffic", default = None,
-                        help = "Displays Current Traffic")
+    parser.add_argument("-w", "--watch", type=str, nargs="?",
+                        metavar="watch_traffic", default=None,
+                        help="Displays Current Traffic")
 
-    parser.add_argument("-l", "--log", type = str, nargs = 1,
-                        metavar = "log_traffic", default = None,
-                        help = "Logs Current Traffic")
+    parser.add_argument("-l", "--log", type=str, nargs="?",
+                        metavar="log_traffic", default=None,
+                        help="Logs Current Traffic")
 
-    parser.add_argument("-d", "--dome", type = str, nargs = 1,
-                        metavar = "enables_rules", default = None,
-                        help = "Enables the actual IDS/IPS rules")
-    
+    parser.add_argument("-d", "--dome", type=str, nargs="?",
+                        metavar="enables_rules", default=None,
+                        help="Enables the actual IDS/IPS rules")
+
     args = parser.parse_args()
+
+    # Check if any arguments were provided
+    if not any(vars(args).values()):
+        parser.print_help()
+        sys.exit(0)
 
 
 if __name__ == "__main__":
