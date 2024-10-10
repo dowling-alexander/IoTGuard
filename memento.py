5aea8f3c15afb23d1ef2e157e1024ac9cb58cc8f
dowling-alexander <dowling.alexander.e@gmail.com>
2024-10-10 21:48:51 +1100

    Update memento.py
    updating for basic log functionality
    need to work on a nice way on how to log and stop logging.

--- memento.py
+++ memento.py
@@ -1,2 +1,6 @@
 #was in a latin mood for these naming conventions. Will come back to this. use scapy to record files to a .pcap
-import scapy
+from scapy.all import sniff, wrpcap
+
+def log_packets():
+  packets =sniff(count=10)
+  wrpcap('packets.pcap', packets)
